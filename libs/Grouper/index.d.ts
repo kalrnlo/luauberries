1d44fec669b508019e1befdcf727153eb8eacefd
kalrnlo <azgoodlo95@gmail.com>
2024-08-04 16:52:17 -0400

    Update config stuff

--- libs/Grouper/index.d.ts
+++ libs/Grouper/index.d.ts
@@ -1,8 +1,9 @@
 /// <reference types="@rbxts/types" />
 /// <reference types="@rbxts/compiler-types" />
+
+type RankRange = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60 | 61 | 62 | 63 | 64 | 65 | 66 | 67 | 68 | 69 | 70 | 71 | 72 | 73 | 74 | 75 | 76 | 77 | 78 | 79 | 80 | 81 | 82 | 83 | 84 | 85 | 86 | 87 | 88 | 89 | 90 | 91 | 92 | 93 | 94 | 95 | 96 | 97 | 98 | 99 | 100 | 101 | 102 | 103 | 104 | 105 | 106 | 107 | 108 | 109 | 110 | 111 | 112 | 113 | 114 | 115 | 116 | 117 | 118 | 119 | 120 | 121 | 122 | 123 | 124 | 125 | 126 | 127 | 128 | 129 | 130 | 131 | 132 | 133 | 134 | 135 | 136 | 137 | 138 | 139 | 140 | 141 | 142 | 143 | 144 | 145 | 146 | 147 | 148 | 149 | 150 | 151 | 152 | 153 | 154 | 155 | 156 | 157 | 158 | 159 | 160 | 161 | 162 | 163 | 164 | 165 | 166 | 167 | 168 | 169 | 170 | 171 | 172 | 173 | 174 | 175 | 176 | 177 | 178 | 179 | 180 | 181 | 182 | 183 | 184 | 185 | 186 | 187 | 188 | 189 | 190 | 191 | 192 | 193 | 194 | 195 | 196 | 197 | 198 | 199 | 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 209 | 210 | 211 | 212 | 213 | 214 | 215 | 216 | 217 | 218 | 219 | 220 | 221 | 222 | 223 | 224 | 225 | 226 | 227 | 228 | 229 | 230 | 231 | 232 | 233 | 234 | 235 | 236 | 237 | 238 | 239 | 240 | 241 | 242 | 243 | 244 | 245 | 246 | 247 | 248 | 249 | 250 | 251 | 252 | 253 | 254 | 255
+
 /**
- * # Grouper
- *
  * Used for getting the latest group rank or role of a player on the server.
  */
 declare namespace grouper {
@@ -11,7 +12,7 @@ declare namespace grouper {
 	type ReplicateRankUpdateCallback = (player: Player, rank: number, role: string) => void;
 	type RankChangedCallback = (player: Player, new_rank: number, old_rank: number) => void;
 	export type ClientConfig = {
-		connect_rank_changed: (callback: ReplicateRankUpdateCallback) -> void
+		connect_rank_changed: (callback: ReplicateRankUpdateCallback) => void
 	};
 	export type ServerConfig = {
 		fire_all_rank_changed: undefined;
@@ -25,15 +26,37 @@ declare namespace grouper {
 		groupid: number;
 	};
 	
-    export function get_rank_and_role(player: Player): LuaTuple<[number, string]>;
+	/**
+	* Returns a players rank and role, note: this is faster than doing grouper.rank and grouper.role
+	* but that optimization doesn't really matter so this is more of just a shorthand
+ 	*/
+    export function rank_and_role(player: Player): LuaTuple<[number, string]>;
+	/**
+	* Attaches a callback to be ran whenever a players rank is changed
+ 	*/
 	export function on_rank_changed(callback: RankChangedCallback): () => void;
+	/**
+	* Returns a map with every rank mapped to its role
+ 	*/
+	export function roles(): Map<RankRange, string | undefined>;
     export function is_in_group(player: Player): boolean;
+	/**
+	* Returns the current rank for the player
+ 	*/
 	export function rank(player: Player): number;
+	/**
+	* Returns the current role for the player
+ 	*/
     export function role(player: Player): string;
+	/**
+	* Returns a map of players to their current ranks
+ 	*/
+	export function players(): Map<Player, number>;
 	export namespace init {
 		export function client(conifg: ClientConfig): void;
 		export function server(conifg: ServerConfig): void;
-	};
+	}
     export {};
 }
+
 export = grouper;
